74430d52ea103b6e140abcd31de98dd8e1250e0a
Michal-MK <michalhz159@gmail.com>
2021-11-28 21:30:33 +0100

    Added H2 dependencies and first test - more test preparation and first PoC test - added 3 restrictions per category as test samples

--- ehealth-restrictions/src/main/java/org/ehealth/restrictions/processing/RestrictionProcessorImpl.java
+++ ehealth-restrictions/src/main/java/org/ehealth/restrictions/processing/RestrictionProcessorImpl.java
@@ -15,19 +15,13 @@ public class RestrictionProcessorImpl implements RestrictionProcessor {
 	@Override
 	@NotNull
 	public List<Restriction> process(PatientMedRecord record) {
-		if (record.certificates.isEmpty() && record.tests.isEmpty()) {
-			return Restriction.find("u.scope = @global or u.scope = @nonvax",
-					Map.of("global", RestrictionScope.GLOBAL,
-							"nonvax", RestrictionScope.NOT_VACCINATED))
-					.list();
-		}
 		return List.of();
 	}
 
 	@Override
 	public List<Restriction> getGlobalRestrictions() {
-		return Restriction.find("u.scope = @global",
-				Map.of("global", RestrictionScope.GLOBAL))
+		return Restriction.find("scope = :scope",
+				Map.of("scope", RestrictionScope.GLOBAL))
 				.list();
 	}
 }
--- ehealth-restrictions/src/test/java/org/ehealth/restrictions/processing/RestrictionProcessorTest.java
+++ ehealth-restrictions/src/test/java/org/ehealth/restrictions/processing/RestrictionProcessorTest.java
@@ -1,17 +1,90 @@
 package org.ehealth.restrictions.processing;
 
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.h2.H2DatabaseTestResource;
+import io.quarkus.test.junit.QuarkusTest;
 import org.ehealth.restrictions.entities.Restriction;
+import org.ehealth.restrictions.entities.RestrictionScope;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
+@QuarkusTest
+@QuarkusTestResource(H2DatabaseTestResource.class)
+@Transactional
 public class RestrictionProcessorTest {
+
 	@Inject
 	RestrictionProcessor processor;
 
 	@BeforeEach
-	void setup() {
+	void setup(){
 		Restriction.deleteAll();
+	}
+
+	@Test
+	void retrieveGlobalRestrictionsOnly() {
+		Restriction.persist(getRestrictions());
+
+		List<Restriction> globals = processor.getGlobalRestrictions();
+
+		assertThat(globals.stream().map(Restriction::getTitle)).contains("1", "2", "3");
+	}
+
+	List<Restriction> getRestrictions() {
+		return List.of(
+				new Restriction("1", "Description 1",
+						LocalDate.of(2021, Month.APRIL, 20),
+						LocalDate.of(2021, Month.MAY, 20), RestrictionScope.GLOBAL),
+				new Restriction("2", "Description 2",
+						LocalDate.of(2021, Month.APRIL, 20),
+						LocalDate.of(2021, Month.MAY, 20), RestrictionScope.GLOBAL),
+				new Restriction("3", "Description 3",
+						LocalDate.of(2021, Month.APRIL, 20),
+						LocalDate.of(2021, Month.MAY, 20), RestrictionScope.GLOBAL),
+				new Restriction("4", "Description 4",
+						LocalDate.of(2021, Month.APRIL, 20),
+						LocalDate.of(2021, Month.MAY, 20), RestrictionScope.NOT_VACCINATED),
+				new Restriction("5", "Description 5",
+						LocalDate.of(2021, Month.APRIL, 20),
+						LocalDate.of(2021, Month.MAY, 20), RestrictionScope.NOT_VACCINATED),
+				new Restriction("6", "Description 6",
+						LocalDate.of(2021, Month.APRIL, 20),
+						LocalDate.of(2021, Month.MAY, 20), RestrictionScope.NOT_VACCINATED),
+				new Restriction("7", "Description 7",
+						LocalDate.of(2021, Month.APRIL, 20),
+						LocalDate.of(2021, Month.MAY, 20), RestrictionScope.POST_CONTRACTION),
+				new Restriction("8", "Description 8",
+						LocalDate.of(2021, Month.APRIL, 20),
+						LocalDate.of(2021, Month.MAY, 20), RestrictionScope.POST_CONTRACTION),
+				new Restriction("9", "Description 9",
+						LocalDate.of(2021, Month.APRIL, 20),
+						LocalDate.of(2021, Month.MAY, 20), RestrictionScope.POST_CONTRACTION),
+				new Restriction("10", "Description 10",
+						LocalDate.of(2021, Month.APRIL, 20),
+						LocalDate.of(2021, Month.MAY, 20), RestrictionScope.VACCINATED),
+				new Restriction("11", "Description 11",
+						LocalDate.of(2021, Month.APRIL, 20),
+						LocalDate.of(2021, Month.MAY, 20), RestrictionScope.VACCINATED),
+				new Restriction("12", "Description 12",
+						LocalDate.of(2021, Month.APRIL, 20),
+						LocalDate.of(2021, Month.MAY, 20), RestrictionScope.VACCINATED),
+				new Restriction("13", "Description 13",
+						LocalDate.of(2021, Month.APRIL, 20),
+						LocalDate.of(2021, Month.MAY, 20), RestrictionScope.TESTED),
+				new Restriction("14", "Description 14",
+						LocalDate.of(2021, Month.APRIL, 20),
+						LocalDate.of(2021, Month.MAY, 20), RestrictionScope.TESTED),
+				new Restriction("15", "Description 15",
+						LocalDate.of(2021, Month.APRIL, 20),
+						LocalDate.of(2021, Month.MAY, 20), RestrictionScope.TESTED)
+		);
 	}
 }
