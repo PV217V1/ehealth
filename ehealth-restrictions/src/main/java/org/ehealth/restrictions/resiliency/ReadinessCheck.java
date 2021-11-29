6dbe1bb0c5c9c10277c790c1f4ba4b3673074c5e
Michal-MK <michalhz159@gmail.com>
2021-11-29 21:56:48 +0100

    Added update for RestrictionsResource - added metric collection - implemented ReadinessCheck

---
+++ ehealth-restrictions/src/main/java/org/ehealth/restrictions/resiliency/ReadinessCheck.java
@@ -0,0 +1,25 @@
+package org.ehealth.restrictions.resiliency;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+import org.ehealth.restrictions.entities.Restriction;
+
+import java.util.Random;
+
+@Readiness
+public class ReadinessCheck implements HealthCheck {
+
+	@Override
+	public HealthCheckResponse call() {
+		try {
+			Restriction.count();
+		} catch (Exception e) {
+			return HealthCheckResponse.down("database down");
+		}
+		if (new Random().nextInt(100) > 95) {
+			return HealthCheckResponse.down("dead");
+		}
+		return HealthCheckResponse.up("alive");
+	}
+}
--- ehealth-restrictions/src/main/java/org/ehealth/restrictions/resources/RestrictionsResource.java
+++ ehealth-restrictions/src/main/java/org/ehealth/restrictions/resources/RestrictionsResource.java
@@ -1,6 +1,8 @@
 package org.ehealth.restrictions.resources;
 
 import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
@@ -15,7 +17,6 @@ import org.ehealth.restrictions.endpoints.dto.medtests.MedTestDTO;
 import org.ehealth.restrictions.endpoints.dto.patients.PatientDTO;
 import org.ehealth.restrictions.processing.RestrictionProcessor;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
-import org.jboss.resteasy.annotations.jaxrs.QueryParam;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
@@ -23,6 +24,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -52,6 +54,8 @@ public class RestrictionsResource {
 	@POST
 	@Path("/create")
 	@Transactional
+	@Counted(name = "restrictions.createCalls", description = "How many times this endpoint was called.")
+	@Timed(name = "restrictions.createDuration", description = "How long does it take to persist a restriction.")
 	public Response create(Restriction item) {
 		item.persist();
 		return Response.accepted(item)
@@ -80,6 +84,22 @@ public class RestrictionsResource {
 				.build();
 	}
 
+	@PUT
+	@Path("/{id}/update")
+	public Response update(@PathParam Long id, Restriction newValue) {
+		Restriction found = Restriction.findById(id);
+
+		if (found == null) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+
+		found.setTitle(newValue.getTitle());
+		found.setDescription(newValue.getDescription());
+		found.setExpired(newValue.getExpired());
+
+		return Response.ok(found).build();
+	}
+
 
 	@GET
 	@Path("/forUser/{id}")
@@ -93,6 +113,8 @@ public class RestrictionsResource {
 							"with provided certificates and tests must follow!")
 	})
 	@Fallback(fallbackMethod = "forUserFallback")
+	@Counted(name = "restrictions.forUserCalls", description = "How many times this endpoint was called.")
+	@Timed(name = "restrictions.forUserDuration", description = "How long does it take to lookup restrictions for users.")
 	public Response forUser(@PathParam Long id) {
 		PatientDTO p;
 		List<MedCertificateDTO> medCerts;
@@ -127,9 +149,11 @@ public class RestrictionsResource {
 	}
 
 	@DELETE
-	@Path("delete")
+	@Path("/{id}/delete")
 	@Transactional
-	public Response delete(@QueryParam Long id) {
+	@Counted(name = "restrictions.deleteCalls", description = "How many times this endpoint was called.")
+	@Timed(name = "restrictions.deleteDuration", description = "How long does it take to delete a restriction.")
+	public Response delete(@PathParam Long id) {
 		boolean res = Restriction.deleteById(id);
 		return res ? Response.ok().build() : Response.noContent().build();
 	}
