89b015b32c4e0db1b1d8aa00d436b79077253f72
Michal-MK <michalhz159@gmail.com>
2021-12-01 20:34:12 +0100

    Added Converter for LocalDateTime for JAX-RS - added attempt at Grafana Dashboard - removed extra qualifiers in application.properties as they are unnecessary?

---
+++ ehealth-restrictions/src/main/java/org/ehealth/restrictions/resources/converters/LocalDateConverterProvider.java
@@ -0,0 +1,20 @@
+package org.ehealth.restrictions.resources.converters;
+
+import javax.ws.rs.ext.ParamConverter;
+import javax.ws.rs.ext.ParamConverterProvider;
+import javax.ws.rs.ext.Provider;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+
+@Provider
+public class LocalDateConverterProvider  implements ParamConverterProvider {
+
+    @Override
+    public <T> ParamConverter<T> getConverter(Class<T> forClass, Type type, Annotation[] annotations) {
+        if(forClass.equals(LocalDate.class)){
+            return (ParamConverter<T>) new LocalDateConverter();
+        }
+        return null;
+    }
+}
