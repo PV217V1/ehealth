866b8f1b0f9449fec33f34a81f458f6d6fa12be4
yanno <ban.yanno@gmail.com>
2021-12-03 23:21:21 +0100

    change import.sql table e_patient to person

--- ehealth-patients/src/main/resources/import.sql
+++ ehealth-patients/src/main/resources/import.sql
@@ -1,5 +1,6 @@
---INSERT INTO e_patient(id, name, age, address, phoneNum) VALUES (nextval('hibernate_sequence'), 'Tom', 2, 'address 1', NULL);
---INSERT INTO e_patient(id, name, age, address, phoneNum) VALUES (nextval('hibernate_sequence'), 'Jerry', 8, 'address 1', NULL);
---INSERT INTO e_patient(id, name, age, address, phoneNum) VALUES (nextval('hibernate_sequence'), 'The Dog', 10, 'address 2', NULL);
---INSERT INTO e_patient(id, name, age, address, phoneNum) VALUES (nextval('hibernate_sequence'), 'The Lady', 60, 'address 3', '012345678');
---INSERT INTO e_patient(id, name, age, address, phoneNum) VALUES (nextval('hibernate_sequence'), 'Are there any more actors?', 99, 'Hello', '000000000');
+INSERT INTO persion(id, name, age, address, telephone,email) VALUES (nextval('hibernate_sequence'), 'Tom', 2, 'address 1', NULL,NULL);
+INSERT INTO e_patient(id, name, age, address, telephone,email) VALUES (nextval('hibernate_sequence'), 'Jerry', 8, 'address 1', NULL,NULL);
+INSERT INTO e_patient(id, name, age, address, telephone,email) VALUES (nextval('hibernate_sequence'), 'The Dog', 10, 'address 2', NULL,NULL);
+INSERT INTO e_patient(id, name, age, address, telephone,email) VALUES (nextval('hibernate_sequence'), 'The Lady', 60, 'address 3', '012345678');
+INSERT INTO e_patient(id, name, age, address, telephone,email) VALUES (nextval('hibernate_sequence'), 'Are there any more actors?', 99, 'Hello', '000000000',NULL);
+
